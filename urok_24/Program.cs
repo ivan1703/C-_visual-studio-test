c9a6bbb85ba5174121d57e3327b1e01fc3d4bb5f
ivan1703 <iwan.balaschow228@mail.ru>
2017-10-14 21:59:57 +0300

    все удалил и написал новый код

--- urok_24/Program.cs
+++ urok_24/Program.cs
@@ -7,25 +7,16 @@ using System.Threading.Tasks;
 
 namespace urok_24
 {
-    enum breat {Dog,cat,bird,bear,Tiger};
-    class Animal {
-        public breat breat;
-       public Animal(breat breat){
-            this.breat = breat;
-        }
-        public void print() {
-            Console.WriteLine(breat);
-        }
-    }
+   
+        
+    
     class Program
     {
         static void Main(string[] args)
         {
-          
-            Animal dog = new Animal(breat.cat);
-            dog.print();
-            Console.ReadKey();
-
+            Console.WriteLine("Hallo, world");
+            Console.ReadKey();     
+            
         }
     }
 }
