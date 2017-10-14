2863d3a8df63b5da61a2f711b8c14da52ef14efc
ivan1703 <iwan.balaschow228@mail.ru>
2017-10-14 21:55:22 +0300

    Добавьте файлы проекта.

---
+++ urok_24/Program.cs
@@ -0,0 +1,30 @@
+﻿//в 24 уроке учим перечисления enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace urok_24
+{
+    enum breat {Dog,cat,bird,bear,Tiger};
+    class Animal {
+        public breat breat;
+       public Animal(breat breat){
+            this.breat = breat;
+        }
+        public void print() {
+            Console.WriteLine(breat);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal(breat.cat);
+            dog.print();
+            Console.ReadKey();
+
+        }
+    }
+}
